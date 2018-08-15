d286662cfb9d6047f1fddc18f0978a0cd28f743c
zerogpm <zerogpm@gmail.com>
2018-08-15 15:43:00 -0700

    Mongoose Queries and ID Validation

---
+++ playground/mongodb-queries.js
@@ -0,0 +1,32 @@
+const {ObjectID} = require('mongodb');
+let {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+let id = '5b73afb5d8e111c425555971';
+
+if (!ObjectID.isValid(id)) {
+  return console.log('ID not valid');
+}
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo)
+// });
+
+// Todo.findById({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo)
+// }).catch((err) => console.log(err));
+
+User.findById({
+  _id: id
+}).then((user) => console.log('User', user)).catch((err) => console.log(err));
