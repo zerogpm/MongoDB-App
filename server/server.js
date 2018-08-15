042b77a23260bedef4cfb5aae10db49fceb9db01
zerogpm <zerogpm@gmail.com>
2018-08-15 14:03:15 -0700

    Testing Get /todos

--- server/server.js
+++ server/server.js
@@ -30,8 +30,10 @@ app.get('/todos', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
-});
+if(!module.parent){
+  app.listen(3000, () => {
+    console.log('Started on port 3000');
+  });
+}
 
 module.exports = {app};
