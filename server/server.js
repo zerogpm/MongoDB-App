5aa711b9286e2b7a1f4d844ade4b2e7db754931e
zerogpm <zerogpm@gmail.com>
2018-08-15 21:22:38 -0700

    get id result

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -27,6 +28,20 @@ app.get('/todos', (req, res) => {
   }, (err) => {
     res.status(400).send(err);
   });
+});
+
+//GET /todos/id
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send({status:'not valid ID'});
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send({status:'Could not find this ID'});
+    }
+    res.send({todo});
+  }).catch((err) => res.status(400).send({status: 'fail'}));
 });
 
 
