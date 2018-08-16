088a88e86dd532e4067c812281ea64609ff96e41
zerogpm <zerogpm@gmail.com>
2018-08-15 22:05:17 -0700

    Add test cases for get /todo/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,12 +1,13 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [
-  {text: 'First test todo'},
-  {text: 'Second test todo'}
+  {_id: new ObjectID(), text: 'First test todo'},
+  {_id: new ObjectID(), text: 'Second test todo'}
 ];
 
 beforeEach((done) => {
@@ -64,6 +65,31 @@ describe('POST /todos', () => {
           expect(res.body.todos.length).toBe(2);
         })
         .end(done);
+    });
+  });
+
+  describe('GET /todos/:id', () =>{
+    it('should return todo doc', (done) => {
+      request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todo.text).toBe(todos[0].text);
+        })
+        .end(done);
+    });
+
+    it('should return 404 if todo not found', (done) => {
+      let id = new ObjectID();
+      request(app)
+        .get(`/todos/${id}`)
+        .expect(404).end(done);
+    });
+
+    it('should return 404 for non-object ids', (done) => {
+      request(app)
+        .get('/todos/123')
+        .expect(404).end(done);
     });
   });
 });
