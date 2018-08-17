8b620dd97da14ec1dddc3833d250b0a306663768
zerogpm <zerogpm@gmail.com>
2018-08-16 22:19:44 -0700

    Testing patch

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -7,7 +7,7 @@ const {Todo} = require('./../models/todo');
 
 const todos = [
   {_id: new ObjectID(), text: 'First test todo'},
-  {_id: new ObjectID(), text: 'Second test todo'}
+  {_id: new ObjectID(), text: 'Second test todo', completed: true, completedAt: new Date()}
 ];
 
 beforeEach((done) => {
@@ -126,5 +126,42 @@ describe('POST /todos', () => {
           .end(done);
       });
     })
+  });
+});
+
+describe('PATCH /todos/:id', () => {
+  it('should update the todo', (done) => {
+    let hexId = todos[0]._id.toHexString();
+    let text = 'This should be the new text';
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        text,
+        completed: true,
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true)
+      })
+      .end(done);
+  });
+
+  it('should clear completedAt when todo is not completed', (done) => {
+    let hexId = todos[1]._id.toHexString();
+    let text = 'this is second';
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        text,
+        completed: false
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.todo.completedAt).toBeFalsy();
+      })
+      .end(done);
   });
 });
