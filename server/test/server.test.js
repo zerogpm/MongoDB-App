706bf6b9f08c6765f37fcb371edf033732aa2edd
zerogpm <zerogpm@gmail.com>
2018-08-19 17:33:21 -0700

    Add test for pos /users/login

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -205,7 +205,7 @@ describe('POST /users', () => {
         expect(user).toBeTruthy();
         expect(user.password).not.toBe(password);
         done();
-      })
+      }).catch((e) => done(e));
     });
   });
 
@@ -225,5 +225,46 @@ describe('POST /users', () => {
       .send({email: users[0].email, password: users[0].password})
       .expect(400)
       .end(done);
+  });
+});
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({email: users[1].email, password: users[1].password})
+      .expect(200)
+      .expect((res) => {
+        expect(res.header['x-auth']).toBeTruthy();
+      }).end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0].token).toBe(res.header['x-auth']);
+          done();
+        }).catch((e) => done(e));
+    });
+  });
+
+  it('should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({email: users[1].email, password: 'invalid email'})
+      .expect(400)
+      .expect((res) => {
+        expect(res.header['x-auth']).toBeFalsy();
+      })
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
   });
 });
